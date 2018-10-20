6f390f203ab4d106519344572875596fef750673
develop <xuhaonju@gmail.com>
2018-10-21 00:08:08 +0200

    update

--- ifft_test.py
+++ ifft_test.py
@@ -1,21 +1,23 @@
 '''
 test on fft and ifft for image
 '''
-'''
-command groups:
-1) 
-
-'''
-
-
-
-
-
 import numpy as np
+import skimage.external.tifffile as tf
+from matplotlib import pyplot as plt
+
 
 def fft_result(image):
-    xd, yd = image.shape
-    fft_value = np.fft.fft2(image)
-    return fft_value, xd, yd
+    image_fft = np.fft.fftn(image)
 
-def ifft_result(fft_image):
+    return image_fft
+
+def fft_mask(image_fft):
+    image_fft_trans = np.fft.fftshift(image_fft)
+    #plt.imshow(converted_fft, vmin= , vmax= .)
+
+def ifft_result(image_fft):
+    image_restore = np.fft.ifftn(image_fft)
+
+    return np.array(image_restore.real, dtype = np.uint16)
+
+# in the fft_mask part, could manually tune the fft_mask to remove artifical effects.
